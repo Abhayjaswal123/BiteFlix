8bea2ab356cb31e24c93c0d8eb7aef5383b04a67
Abhayjaswal <abhayjaswal312@gmail.com>
2026-04-28 18:26:47 +0530

    Fixed cookies for production, updated CORS and env config

--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -33,7 +33,9 @@ export const sendEmail = async (to, subject, text, html) => {
 
     console.log('Message sent: %s', info.messageId);
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    return info;
   } catch (error) {
     console.error('Error sending email:', error);
+    throw error;
   }
 };
